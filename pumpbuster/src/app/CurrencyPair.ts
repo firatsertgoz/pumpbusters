1c170bf326d24adfec3000e7d551ecd26cd7a7c2
bcanvural <bcanvural@gmail.com>
2017-06-25 13:37:57 +0200

    Fix css

--- pumpbuster/src/app/CurrencyPair.ts
+++ pumpbuster/src/app/CurrencyPair.ts
@@ -62,7 +62,7 @@ export class CurrencyPair {
               let criticalPointPrice   =  ((currentPriceAverage/this.lastPriceAverage) * 100) - 100
               let criticialPointVolume =  ((currentVolumeAverage/this.lastVolumeAverage) * 100) - 100
               console.log('CRITICAL POINT PRICE' + criticalPointPrice)
-              if( criticalPointPrice > 0.03 && criticialPointVolume > 0.03){
+              if( criticalPointPrice > 0.1 && criticialPointVolume > 0.1){
                 //alert("PUMP ALERT FOR " + this.exchangeName)
                 this.callback.callback(this.exchangeName, criticalPointPrice)
               }
