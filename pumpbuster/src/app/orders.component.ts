bc1de7fa878f7b2b5a0012ef1eea820ffefa901c
bcanvural <bcanvural@gmail.com>
2017-07-06 22:02:28 +0200

    Add API key input element

--- pumpbuster/src/app/orders.component.ts
+++ pumpbuster/src/app/orders.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'orders',
@@ -7,5 +8,35 @@ import {Component} from '@angular/core';
 })
 
 export class OrdersComponent {
-    
+
+  _currency: string
+  apiKey: string
+  needApiKey = true
+
+  constructor(private apiService: ApiService) { }
+
+  @Input('selectedCurrency')
+  set currency(changedValue: string) {
+    this._currency = changedValue;
+  }
+
+  saveApiKey(apiKey) {
+    this.apiKey = apiKey;
+    this.needApiKey = false;
+    console.log("APIKEY: " + this.apiKey);
+  }
+
+
+  isOrdersHidden(): boolean {
+    if (this.needApiKey) {
+      return true
+    }
+
+    if (!this._currency) {
+      return true;
+    }
+  }
+
+
+
 }
