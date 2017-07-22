cbcdc182af597e25a063150a76c899b711dfe25d
bcanvural <bcanvural@gmail.com>
2017-07-22 17:39:44 +0200

    Add getavgspreads

--- pumpbuster/server.py
+++ pumpbuster/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, copy_current_request_context
 import json
 import re
 from flask_cors import CORS, cross_origin
@@ -6,12 +6,18 @@ import requests
 import time
 import hmac
 import hashlib
+from configobj import ConfigObj
+from gevent.lock import BoundedSemaphore
+import gevent.monkey
+import random
 
 app = Flask(__name__)
 CORS(app)
 HOST = "127.0.0.1"
 PORT = 5000
 API_SECRET = ""
+config = ConfigObj('configfile')
+currencies = [x["Currency"] for x in requests.get('https://bittrex.com/api/v1.1/public/getcurrencies').json()["result"]] # one linerrrrrr
 
 @app.route('/getmarketsummary', methods=['GET'])
 def get_market_summary():
@@ -28,13 +34,16 @@ def get_market_summary():
 def histohour():
     if request.method == 'GET':
         try:
-            fsym = request.args.get('fysm')	
-            tsym = request.args.get("tsym")	
-            e	 = request.args.get('e')
+            fsym = request.args.get('fsym')	
+            tsym = "BTC"
+            e = request.args.get('e')
             limit = request.args.get('limit')	
-            url = 'https://min-api.cryptocompare.com/data/histohour?fsym=' + fsym + '&tsym=' + tsym +'&limit=' + limit + '&e='e
-            return jsonify({"response":request.get(url).json(),statusCode : 200})
-        except Exception
+            url = 'https://min-api.cryptocompare.com/data/histohour?fsym=' + fsym + '&tsym=' + tsym +'&limit=' + limit + '&e='+e
+            return jsonify({"response":requests.get(url).json(), "statusCode" : 200})
+        except Exception as ex:
+            template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+            message = template.format(type(ex).__name__, ex.args)
+            print(message)
             return jsonify({"response":{}, "statusCode": 404})
 
 @app.route('/getbalance', methods=['GET'])
@@ -49,6 +58,54 @@ def get_balance():
             return jsonify({"response": response, "statusCode": 200})
         except Exception:
             return jsonify({"response": {}, "statusCode": 404})
+
+@app.route('/getcurrencies', methods=['GET'])
+def get_currencies():
+    if request.method == 'GET':
+        try:
+            url = 'https://bittrex.com/api/v1.1/public/getcurrencies'
+            response = requests.get(url).json()
+            currencies = [x["Currency"] for x in response["result"]]
+            return jsonify({"response": currencies, "statusCode": 200})
+        except Exception:
+            return jsonify({"response": {}, "statusCode": 404})
+
+@app.route('/getavgspreads', methods=['GET'])
+def get_avg_spreads():
+    if request.method == 'GET':
+        try:
+            depth = 1 #bittrex depth param
+            N = 5 # consider first N entries in order books
+            SAMPLE = 5 #serve at most SAMPLE results.
+            final_obj = {}
+            semaphore = BoundedSemaphore(1) #mutual exclusion on final_obj
+            random.shuffle(currencies) # shuffle array before taking random samples
+            @copy_current_request_context
+            def fetch_and_write(currency, depth, N, semaphore, final_obj):
+                url = 'https://bittrex.com/api/v1.1/public/getorderbook?market=BTC-' + currency + '&type=both&depth='+ str(depth)
+                try:
+                    response = requests.get(url).json()
+                    bid_list = [bid["Rate"] for bid in response["result"]["buy"]][:N]
+                    bid_avg = sum(bid_list) / len(bid_list)
+                    ask_list = [ask["Rate"] for ask in response["result"]["sell"]][:N]
+                    ask_avg = sum(ask_list) / len(ask_list)
+                    perc_diff = ((ask_avg - bid_avg) / bid_avg ) * 100
+                    semaphore.acquire()
+                    final_obj[currency] = perc_diff
+                    semaphore.release()
+                except Exception as ex:
+                    template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+                    message = template.format(type(ex).__name__, ex.args)
+                    print(message, currency)
+
+            jobs = [gevent.spawn(fetch_and_write, currency, depth, N, semaphore, final_obj) for currency in currencies[:SAMPLE] if currency != 'BTC']
+            gevent.wait(jobs)
+            return jsonify({"response": final_obj, "statusCode": 200})
+            
+        except Exception as ex:
+            template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+            message = template.format(type(ex).__name__, ex.args)
+            print(message)
 
 if __name__ == "__main__":
     app.run(host=HOST, port=PORT, threaded=True)
